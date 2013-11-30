e80a0a8c42af8db1ce5cfed7af78251d83004b6b
kerry95 <95kerry@gmail.com>
2013-11-30 23:06:20 +0800

    Add class 'PanelView', 'ScenePanelView'

---
+++ editor/src/js/view.Panel.js
@@ -0,0 +1,41 @@
+/**
+ * 面板视图
+**/
+define([
+    'jquery', 'underscore', 'backbone'
+], function(
+    $, _, Backbone
+){
+    var PanelView;
+
+    /**
+     * 继承类 `Backbone.View`
+    **/
+    PanelView = Backbone.View.extend({
+        /**
+         * 自定义的初始化方法，会在默认的构造函数中被调用
+         *
+         * @method initialize
+         * @param {Object} [options]
+         *      @param {DOMElement|String} [options.container]
+        **/
+        initialize: function(options){
+            var container;
+
+            options = options || {};
+            if(_.has(options, 'container')){
+                container = options.container
+                // TODO: 改成判断是否为DOMElement
+                if(_.isObject(container)){
+                    this.$container = $(container);
+                }
+                else if(_.isString(container)){
+                    this.$container = $(container);
+                }
+            }
+            if(!this.$container) this.$container = $('body');
+        }
+    });
+
+    return PanelView;
+});
---
+++ editor/src/js/view.panel.Scene.js
@@ -0,0 +1,47 @@
+/**
+ * 景物面板视图
+ * 景物面板相当于flash pro里的编辑栏窗口
+ *
+ * @module view.panel.Scene
+**/
+define([
+    'underscore',
+    'view.Panel', 'tmpl!html/panel.html'
+], function(
+    _,
+    PanelView, panelTmpl
+){
+    var ScenePanelView;
+
+    /**
+     * @class ScenePanelView
+     * @extends PanelView
+    **/
+    ScenePanelView = PanelView.extend({
+        /**
+         * Start: backbone内置属性/方法
+        **/
+        attribute: {
+            'class': 'scenePanel'
+        },
+        initialize: function(options){
+            // 借用父类的`initialize`方法
+            this.constructor.__super__.initialize.apply(this, arguments);
+        },
+        render: function(){
+            this.$el
+                .html( panelTmpl({
+                    title: '景物面板',
+                    type: 'scene'
+                }) )
+                .appendTo(this.$container);
+
+            return this;
+        }
+        /**
+         * End: backbone内置属性/方法
+        **/
+    });
+
+    return ScenePanelView;
+});
