518c9d792e84fea560650328075d12494bdd6158
kerry95 <95kerry@gmail.com>
2013-12-02 16:20:50 +0800

    Add event listener onto model/collection/view in controller module

--- editer/src/js/controller.js
+++ editer/src/js/controller.js
@@ -26,5 +26,37 @@ define([
         scenePanelView.render();
         boneTreePanelView.render();
         timelinePanelView.render();
+
+        // 监听model/collection上的事件
+        boneCollection.on('add', function(model, collection, options){
+            scenePanelView.addBone(model.toJSON());
+        });
+
+        /**
+        Fired when click add button of scene panel
+        @event clickAddBtn
+        @param {String} textureUrl texture image's url
+        @param {Object} [options] optional param
+            @param {Object} [options.addOptions] optional param for adding bone model
+        **/
+        scenePanelView.on('clickAddBtn', function(textureUrl, options){
+            addBoneModel(textureUrl, options && options.addOptions);
+        });
+    };
+
+
+    /**
+    添加一个骨骼model到collection中
+    @private
+    @param <String> textureUrl 骨骼纹理图的url
+    @param <Object> [options] 添加model时的可选参数
+        @param <Boolean> [options.add]
+        @param <Boolean> [options.remove]
+        @param <Boolean> [options.merge]
+    **/
+    function addBoneModel(textureUrl, options){
+        boneCollection.add({
+            textureUrl: textureUrl
+        }, options && options.addOptions);
     };
 });
