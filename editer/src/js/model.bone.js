b6562aa404640926cf6d4a588fcf4ba3769228ba
kerry95 <95kerry@gmail.com>
2014-01-04 19:01:15 +0800

    Bone model support 'toJSON()' with two options: specifying time; whether mixin keyframe data into bone data

--- editer/src/js/model.bone.js
+++ editer/src/js/model.bone.js
@@ -3,13 +3,15 @@
 @module
 **/
 define([
-    'Backbone.Relational', 'relationalScope',
+    'underscore', 'Backbone.Relational', 'relationalScope',
     'modelUtil', 'model.keyframe', 'collection.keyframe', 'collection.Bone'
 ], function(
-    Backbone, relationalScope,
+    _, Backbone, relationalScope,
     util
 ){
-    var BoneModel;
+    var findWhere = _.findWhere,
+        extend = _.extend,
+        BoneModel;
 
     /**
     @class BoneModel
@@ -54,6 +56,34 @@ define([
             id = util.createId();
             console.debug('Create a bone model with id %s', id);
             this.set('id', id);
+        },
+        /**
+        @param {Object} [options]
+            @param {Number} [options.time] 只获取指定时间点的数据
+            @param {Boolean} [options.mixin=true] 是否将关键帧的数据混入到骨骼数据中。只有当设置了 `options.time` 时才有效
+        **/
+        toJSON: function(options){
+            var keyframeData,
+                json;
+
+            options = options || {};
+
+            json = this.constructor.__super__.toJSON.call(this, options);
+            if('time' in options){
+                keyframeData = findWhere(json.keyframes, {time: options.time});
+                if(keyframeData){
+                    delete json.keyframes;
+                    delete keyframeData.id;
+                    delete keyframeData.bone;
+                    extend(json, keyframeData);
+                }
+                else{
+                    // TODO: 支持获取关键帧范围内某个时间点的数据，即指定的时间点不一定是关键帧所在的时间点
+                    throw new Error('No keyframe at specified time');
+                }
+            }
+
+            return json;
         },
         fetch: function(){},
         save: function(){},
